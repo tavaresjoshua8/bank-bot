cd66e7381542a9fcaf7cea28492375657376bd86
Joshua Salcido <tavaresjoshua8@gmail.com>
2023-03-28 00:26:47 -0600

    Comando Estatus Agregado

--- src/commands/estatus.mjs
+++ src/commands/estatus.mjs
@@ -1,4 +1,5 @@
-import User from '../models/user.mjs'
+import Role from '../models/role.mjs';
+import User from '../models/user.mjs';
 import { SlashCommandBuilder } from "discord.js";
 
 export default {
@@ -7,18 +8,38 @@ export default {
         .setDescription('Ve un estatus del banco'),
 
     execute: async (interaction) => {
-        let saldos = [];
+        const allRoles = await Role.findAll();
+        const userRoles = interaction.member.roles.cache;
+
+        let permiso = false;
+        allRoles.forEach(role => {
+            if(userRoles.get(role.id)) {
+                permiso = true;
+            }
+        });
+
+        if(!permiso) {
+            await interaction.reply({content: `No tienes permisos para administrar el banco`, ephemeral: true});
+            return;
+        }
+
+        let content = '';
+        let sum = 0;
 
         const users = await User.findAll();
-        users.forEach(async (user, index) => {
+
+        for (const user of users) {
             let transactions = await user.getTransactions();
             const saldo = transactions.map(transaction => transaction.quantity)
                 .reduce((sum, transaction) => sum + transaction);
 
-            saldos[index] = { user: user.nickname, saldo }
-        });
+            content += `${user.nickname}: $${saldo}\n`;
+            sum += saldo;
+        }
 
-        let message = saldos.reduce((msg, item) => msg + `${item.user}: $${item.saldo}\n`);
-        interaction.reply({content: message, ephemeral: true});
+        content += '======================\n'
+            + `Saldo Land: $${sum}`;
+
+        await interaction.reply({ content, ephemeral: false });
     }
 }
